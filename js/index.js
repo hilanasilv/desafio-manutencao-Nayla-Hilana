2fb90d1cf5a2e8197b44a494ed0861954229511b
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-08-17 02:32:03 -0300

    adiciona o arquivo js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const botaoContrate = document.getElementById('btn-contrate');
+
+botaoContrate.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    alert("Site em manutenção, desculpe o transtorno!");
+});
+
